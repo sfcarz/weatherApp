fbf51ac5306e1473b6513ed47f27ab8ef31339b6
Christopher Rojas <christopherrojas@Christophers-iMac.local>
2020-08-19 01:18:20 -0700

    Not quite done yet. Need Sleep. Will finish before class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherApp
+
+Tasked to create a weather application, where user inputs a city and we grab all related weather info and place in a card to display. Also displaying a 5 day forecast. 
--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 $(document).ready(function () {
-    let $search = $('#search').text().trim();
-    let $submit = $('#submit')
-    console.log($search);
+    let $search = $('#searching');
+    let $submit = $('#submit');
+    let $history = $('#history');
+    let storage = JSON.parse(localStorage.getItem('Search'));
 
-
-    var settings = {
-    "url": "https://api.openweathermap.org/data/2.5/weather?q=san francisco&appid=4d3e71aa350785d4a2efee37a6cbbf4e",
-    "method": "GET",
-    "timeout": 0,
-};
-
-    $.ajax(settings).done(function (response) {
-    // console.log('Open Weather Map', response);
-
+    console.log(storage);
 
     $submit.on('click', function (event) {
-        event.preventDefault();
-        console.log('button Works');
+    event.preventDefault();
+    const search = $search.val();
+    const aTag = $('<a>').addClass('list-group-item list-group-item-action').attr('href: #');
+    localStorage.setItem('Search', JSON.stringify(search));
 
-        localStorage.setItem('Search', JSON.stringify($search));
-    })
+    $history.append(aTag);
+    $search.val('')
+
+    var settings = {
+        "url": `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=4d3e71aa350785d4a2efee37a6cbbf4e`,
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).then(function (response) {
+        console.log(response);
+
+        const inputName = response.name;
+        const date = moment().format('dddd, MMMM Do');
+        const temp = response.main.temp;
+        const tempF = (temp - 273.15) * 1.80 + 32;
+        const humidity = response.main.humidity;
+        const wind = response.wind.speed;
+        const nameH1 = $('<h1>').text(inputName).addClass('card-title');
+        const dateH3 = $('<h3>').text(date);
+        const tempH5 = $('<h5>').text(`Temperature ${tempF.toFixed(2)}`);
+        const humidH5 = $('<h5>').text(`Humidity ${humidity}%`);
+        const windH5 = $('<h5>').text(`Wind Speed: ${wind}`);
+
+        $('#userRequest').append(nameH1, dateH3, tempH5, humidH5, windH5)
 
 
 
+        // console.log(inputName, date, temp, tempF, humidity, wind);
+        // const icon = response.weather.first().last();
+        // console.log(icon);
+    });
 });
 
 
+
+    
+
+
+
+    
 
 
 
